707093e5fd37735d13ec787a4768646518960158
Marius Sørensen Dreyer <mariussd@users.noreply.github.com>
2023-03-29 12:13:52 +0200

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,7 +3,7 @@ import { Link } from "@remix-run/react";
 export default function Index() {
   return (
     <main className="relative flex min-h-screen flex-col bg-white sm:flex sm:items-center sm:justify-center">
-      <h1 className="mb-4">Ottos Filmklubb (WIP)</h1>
+      <h1 className="mb-4">Ottos Filmklubb! (WIP)</h1>
       <nav className="flex flex-col border p-4">
         <Link className="underline" to={"/vote"}>
           Stem
